b0ea7b1390a7c69a8daa57053d781e8f5c952cc5
Veerwhitehat <92624033+Veerwhitehat@users.noreply.github.com>
2022-04-05 20:15:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,46 @@
 Webcam.set({
-    width: 350,
-    height: 300,
-    image_format: "png",
-    png_quality: 90
+  width: 350,
+  height: 300,
+  image_format: "png",
+  png_quality: 90
 });
 camera=document.getElementById("camera");
 Webcam.attach("#camera");
 
 function take_snapshot(){
-    Webcam.snap(function(data_uri) {
-      document.getElementById("result").innerHTML= '<img id="selfie_image" src="'+data_uri+'">';
-    })
+  Webcam.snap(function(data_uri) {
+    document.getElementById("result").innerHTML= '<img id="img" src="'+data_uri+'">';
+  })
 }
 
 console.log("ml5 version",ml5.version);
-classifier= ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/YaKOu4JZ5/",modelLoaded)
+classifier= ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/YaKOu4JZ5/model.json",modelLoaded)
 function modelLoaded(){
 
-console.log("@^ ( !# # !( *!@ !( (! !# # !#");
+console.log("model is loaded");
+
+}
+
+function check(){
+
+img=document.getElementById("img");
+classifier.classify(img,gotResult);
+
+
+}
+
+function gotResult(error,results){
+
+if (error) {
+  
+  console.error(error)
+
+} else {
+
+  console.log(results);
+  document.getElementById("result_object_name").innerHTML=results[0].label;
+  document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+  
+}
 
 }
